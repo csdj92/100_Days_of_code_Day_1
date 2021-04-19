2921743a353f6bd5be411be45d201adb415195f1
Cj Davis <csdj92@gmail.com>
2021-04-19 09:19:19 -0400

    Day 1 project

---
+++ main.py
@@ -0,0 +1,7 @@
+greeting = print('Welcome to band name generator')
+
+city = input("What city did you grow up in?\n")
+
+pet = input("What is the name of your pet?\n")
+
+print("Your band name could be " + city + ' ' + pet)
